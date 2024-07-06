807b64ffc1b19ebe6797043f0cc71644c9225f54
6942690 <156633350+6942690@users.noreply.github.com>
2024-07-06 14:10:55 -0400

    Create e-z.sh

---
+++ e-z.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+response=$(grim -g "$(slurp)" -t jpeg - | curl -sS -X POST -H "key: {YOUR-API-KEY} -F "file=@-" https://api.e-z.host/files)
+
+
+if [ -z "$response" ]; then
+    echo "Error: Upload failed or response is empty."
+    notify-send "Error" "Upload failed or response is empty."
+    exit 1
+fi
+
+
+imageUrl=$(echo "$response" | jq -r '.imageUrl')
+
+if [ "$imageUrl" = "null" ] || [ -z "$imageUrl" ]; then
+    echo "Error: Image URL is empty or null."
+    notify-send "Info:" "Image URL is empty or null."
+    exit 1
+fi
+
+
+echo "$imageUrl" | wl-copy
+
+notify-send "Screenshot uploaded" "URL copied to clipboard: $imageUrl"
+
+exit 0
